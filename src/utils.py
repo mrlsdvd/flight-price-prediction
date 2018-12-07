40cdb9aa5413c7ff29ce08ba1de24861047156e8
Minh-An Quinn <minhanquinn@DN0a226311.SUNet>
2018-12-06 19:54:51 -0800

    updates

--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,19 @@ def get_policy(Q):
     """
     policy = np.argmax(Q, axis=1)
     return policy.tolist()
+
+
+
+def get_policy_4D(Q):
+    """
+    Return the optimal policy using the provided Q function (represented as a matrix).
+    The optimal action a for a state s is the action that with the highest value for
+    that state.
+
+    Arguments:
+        Q (np.array): 4D array 
+    Returns:
+        policy: Matrix of optimal actions, such that optimal action of state (days left, demand level and tickets left) is given by policy[days_left-1, demand_level-100, tickets_left-1]
+    """
+    policy = np.argmax(Q, axis=3)
+    return policy
